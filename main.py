fb6eeb5181d87ab6ec8bbc19988d954575ed5fcc
Hassan Alfaifi <hassanawdahcsi2gmail.com>
2022-09-28 17:38:37 +0300

    first example

---
+++ main.py
@@ -0,0 +1,67 @@
+# int
+# float
+# bool
+# str
+# list
+# tuple
+# set
+# dict
+
+
+value = 15
+"""print(value)
+
+print(type(2 - 2))
+print(type(2 * 2))
+print(type(2.14))
+print(2 ** 2)
+print()
+value *= 2
+print(value)
+
+print(9//4)
+print(round(3.1))
+print(round(3.4))
+print(abs(-20))
+
+print(bin(5))
+print(bin(121))
+print(hex(121))
+print(int('0b101', 2))
+"""
+
+'''a, b, c = 1, 2, 3
+print(a)
+print(b)
+print(c)
+
+print(type("Hello There Where are you")'''
+
+'''long_str = Hello There 
+Where 
+are 
+you'''
+
+# print(long_str)
+
+# int_value = int(str(100))
+# print(type(int_value))
+
+# name = 'Hassan '
+# age = 33;
+# print(f'hi {name} . You are {age} years old')
+# print('hi {1} . You are {0} years old'.format('Hassan', 21))
+
+# str_value = 'Hassan alfaifi'
+# print(str_value[0::2])
+# print(str_value[:5])
+#
+# str1 = "welcome to our bootcamp!";
+# print(str1.find('t'))
+# print(str1.find('o', 10))
+# print(str1[::-2])
+
+
+number = input("Enter the number\n")
+number2 = input("Enter the number\n")
+print(int(number) + int(number2))
