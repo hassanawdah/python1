e09f33ad89e902516bcc59e4f0f39c1b8abd1904
Hassan Alfaifi <hassanawdahcsi2gmail.com>
2022-09-28 18:30:34 +0300

    second change

---
+++ example2.py
@@ -0,0 +1,19 @@
+# username = input("Enter you username")
+# password = input("Enter you password")
+#
+# length = len(password)
+# print(f'{username},your password is {"*" * length} and the length is {length} ')
+
+first_list = [1, 2, 3, True, "Hassan"]
+print(first_list[0])
+print(first_list[3])
+
+list = [1, 2, 3, 4, 5, 6, 7, 8]
+print(list[0:2])
+
+for x1 in list:
+    print(x1)
+
+    print(len(list))
+
+    print(4 in list)
